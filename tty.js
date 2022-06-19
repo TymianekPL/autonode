0e9c0edbb6d1e82a8943fb9c849f6b74fe6fa44b
tymianekpl <wilku.tymon@gmail.com>
2022-06-19 10:38:58 +0200

    🩹 Fixed double buffering

--- tty.js
+++ tty.js
@@ -10,17 +10,7 @@ module.exports = {
      enable: () => {
           // enable tty
           const readStream = process.stdin;
-          const writeStream = process.stdout;
           readStream.setRawMode(true);
-          readStream.resume();
-          readStream.setEncoding("utf8"); // set encoding to utf8 for proper handling of special characters
-          readStream.on("data", (key) => { // since we are in raw mode, node won't terminate the process on ctrl+c
-               if (key === '\u0003') { // if ctrl+c
-                    log.info("CTRL+C was pressed, exiting...");
-                    process.exit();
-               }
-          });
-
      },
      check: () => {
           return tty.isatty(process.stdin.fd);
